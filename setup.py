0098337a851f2c4b559697d552530028de3a0be1
alexandr <as.grom@gmail.com>
2020-10-30 04:03:26 +0300

    добавил setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='applogger',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Alexandr Gromkov',
+    author_email='',
+    description='Логгер для модулей.'
+)
